7845719b2a0162acc91f2638ebf4ad9575961986
Joshua Owens <s189263@uos.ac.ukssh-keygenssh-keygenssh-keygen2>
2019-11-12 10:07:43 +0000

    g
    g

--- Jobs.php
+++ Jobs.php
@@ -1,10 +1,10 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Jobs</title>
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+  <title>Jobs</title>
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
   
-	<meta charset="utf-8">
+  <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
       <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
@@ -13,17 +13,14 @@
 <link a href="ER.css" rel = "stylesheet">
 </head>
 <body>
-
  <div class="col-md-12">
-	<br>
-
+  <br>
 <div class="bs-example">
     <nav class="navbar navbar-expand-md navbar-light bg-light">
        
         <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#navbarCollapse">
             <span class="navbar-toggler-icon"></span>
         </button>
-
         <div class="collapse navbar-collapse" id="navbarCollapse">
             <div class="navbar-nav">
                 <a href="Jobs.php" class="nav-item nav-link active">Jobs</a>
@@ -37,54 +34,31 @@
         </div>
     </nav>
 </div>
-
 <img src ="Aseets/Hero2.jpg" style="width: 100%">
-
-
-
 </div>
-
 <div id = "lowerbody">
-
         <div class="col-md-4">
         
         <h1>
           2
         </h1>
-
         </div>
-
-
         <div class="col-md-4">
-
           <h1>
           2
         </h1>
   
         </div>
-
-
         <div class="col-md-4">
-
           <h1>
           2
         </h1>
         
         </div>
-
 </div>
           
-
-
-
 <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anony mous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 </body>
-
-
-
-
-
-
 </html>
