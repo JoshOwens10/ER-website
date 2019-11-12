0fbdfe6417ecc1d2820d85f1a79936670db75b02
Joshua Owens <s189263@uos.ac.ukssh-keygenssh-keygenssh-keygen2>
2019-11-12 23:40:36 +0000

    nav adn box

--- Jobs.php
+++ Jobs.php
@@ -1,22 +1,16 @@
-  
 <!DOCTYPE html>
 <html>
 <head>
   <title>Jobs</title>
   <meta charset="utf-8">
   
-
-
-  <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
-
+  <script src="https://kit.fontawesome.com/13c74952de.js" crossorigin="anonymous"></script>
   <link rel="stylesheet" type="text/css" href="ER1.css">
 </head>
 <body>
-
   <header>
     <nav>
       <ul>
-
        <li> <a href="Jobs.php" class ="active">Jobs </a> </li>
        <li> <a href="CarrerAdvice.php">carrer Advice </a> </li>
        <li> <a href="CompanyProfiles.php">Company Profiles </a> </li>
@@ -27,51 +21,32 @@
  </div>
 </header>
 
+<div id ="jobS">
 
-
-<div class="col-md-12">
-  
-</div>
 <img src ="Aseets/Hero2.jpg" style="width: 100%">
-</div>
-<div id = "lowerbody">
-  <div class ="row">
+search:
+<input type ="text" name ="srch" id ="srch" list="datalist"/>
+<datalist id ="datalist">
+<option value="Berkshire">
+<option value="Buckinghamshire">
+<option value="City of London">
+<option value="Cambridgeshire">
+<option value="Bedfordshire">
+<option value="Dorset">
+<option value="Essex">
+<option value="Suffolk">
+<option value="Cumbria">
+<option value="Somerset">
 
-    <div class="col-md-4">
-      
-      <h1>
-        2
-      </h1>
-    </div>
-    <div class="col-md-4">
-      <h1>
-        2
-      </h1>
-      
-    </div>
-    <div class="col-md-4">
-      <h1>
-        2
-      </h1>
-      
-    </div>
-  </div>
 </div>
+
+
+
 
 <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anony mous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 </body>
 </html>
-
-
-
 </body>
 </html>
-
-
-
-
-
-
-
--- Login.php
+++ Login.php
@@ -1,89 +1,56 @@
 <!DOCTYPE html>
 <html>
 <head>
-  <title>Login/Signup</title>
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-  
+  <title>Jobs</title>
   <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-      <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-      <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-<link a href="ER.css" rel = "stylesheet">
+  
+  <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
+  <link rel="stylesheet" type="text/css" href="ER1.css">
 </head>
 <body>
-
- <div class="col-md-12">
-  <br>
+  <header>
+    <nav>
+      <ul>
+       <li> <a href="Jobs.php">Jobs </a> </li>
+       <li> <a href="CarrerAdvice.php">carrer Advice </a> </li>
+       <li> <a href="CompanyProfiles.php">Company Profiles </a> </li>
+       <li> <a href="ContactUs.php">Contact us </a> </li>
+       <li> <a href="Login.php"class ="active">Login/Signup </a> </li>
+     </ul>
+   </nav>
+ </div>
+</header>
+<div class="col-md-12">
   
-<div class="bs-example">
-    <nav class="navbar navbar-expand-md navbar-light bg-light">
-        <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#navbarCollapse">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-
-        <div class="collapse navbar-collapse" id="navbarCollapse">
-            <div class="navbar-nav">
-                <a href="Jobs.php" class="nav-item nav-link">Jobs</a>
-                <a href="CarrerAdvice.php" class="nav-item nav-link">carrer Advice</a>
-                <a href="CompanyProfiles.php" class="nav-item nav-link">Company Profiles</a>
-                <a href="ContactUs.php" class="nav-item nav-link">Contact us</a>
-            </div>
-            <div class="navbar-nav ml-auto">
-                <a href="Login.php" class="nav-item nav-link active">Login</a>
-            </div>
-        </div>
-    </nav>
 </div>
-
 <img src ="Aseets/Hero2.jpg" style="width: 100%">
-
-
-
 </div>
-
 <div id = "lowerbody">
-
-        <div class="col-md-4">
-        
-        <h1>
-          2
-        </h1>
-
-        </div>
-
-
-        <div class="col-md-4">
-
-          <h1>
-          2
-        </h1>
-  
-        </div>
-
-
-        <div class="col-md-4">
-
-          <h1>
-          2
-        </h1>
-        
-        </div>
-
+  <div class ="row">
+    <div class="col-md-4">
+      
+      <h1>
+        2
+      </h1>
+    </div>
+    <div class="col-md-4">
+      <h1>
+        2
+      </h1>
+      
+    </div>
+    <div class="col-md-4">
+      <h1>
+        2
+      </h1>
+      
+    </div>
+  </div>
 </div>
-          
-
-
-
 <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anony mous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 </body>
-
-
-
-
-
-
+</html>
+</body>
 </html>
